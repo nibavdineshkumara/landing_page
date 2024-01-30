5003343461e75b7c33f5bab3344fc132c6674436
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-01-30 07:07:01 +0530

    [Dinesh] changes the theme to black

--- src/components/theme/theme.ts
+++ src/components/theme/theme.ts
@@ -9,27 +9,27 @@ declare module "@mui/joy/styles" {
     componentBg: string;
   }
 }
-// const primary = {
-//   "50": "#a6e1fa",
-//   "100": "#80c4e6",
-//   "200": "#5aa6d1",
-//   "300": "#0e6ba8",
-//   "400": "#0c488d",
-//   "500": "#0b3680",
-//   "600": "#0a2472",
-//   "700": "#052064",
-//   "800": "#001c55",
-//   "900": "#00072d",
-// };
+const primary = {
+  "50": "#fafafa",
+  "100": "#f5f5f5",
+  "200": "#9e9e9e",
+  "300": "#424242",
+  "400": "#212121",
+  "500": "#000000",
+  "600": "#000000",
+  "700": "#000000",
+  "800": "#000000",
+  "900": "#000000",
+};
 export const muiTheme = extendMuiTheme({
   // This is required to point to `var(--joy-*)` because we are using `CssVarsProvider` from Joy UI.
   cssVarPrefix: "joy",
   colorSchemes: {
     light: {
       palette: {
-        // primary: {
-        //   main: primary[500],
-        // },
+        primary: {
+          main: primary[500],
+        },
         grey: colors.grey,
         error: {
           main: colors.red[500],
@@ -56,9 +56,9 @@ export const muiTheme = extendMuiTheme({
     },
     dark: {
       palette: {
-        // primary: {
-        //   main: primary[600],
-        // },
+        primary: {
+          main: primary[600],
+        },
         grey: colors.grey,
         error: {
           main: colors.red[600],
@@ -90,7 +90,7 @@ const joyTheme = extendJoyTheme({
   colorSchemes: {
     light: {
       palette: {
-        // primary: { ...primary },
+        primary: { ...primary },
         background: {
           appBody: "var(--joy-palette-primary-50)",
           componentBg: "var(--joy-palette-common-white)",
@@ -99,7 +99,7 @@ const joyTheme = extendJoyTheme({
     },
     dark: {
       palette: {
-        // primary: { ...primary },
+        primary: { ...primary },
         background: {
           appBody: "var(--joy-palette-common-black)",
           componentBg: "var(--joy-palette-primary-900)",
