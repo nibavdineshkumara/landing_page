c75e001e01128e5e361e8c1fa3dacbc88ff5bfc2
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-01-31 11:33:17 +0530

    [Dinesh] adds all sections into home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,6 +2,13 @@ import Sheet from "@mui/joy/Sheet";
 import { Section1 } from "./Section1";
 import { Section2 } from "./Section2";
 import { Section3 } from "./Section3";
+import { Section4 } from "./Section4";
+import { Section5 } from "./Section5";
+import { Section6 } from "./Section6";
+import { Section7 } from "./Section7";
+import { Section8 } from "./Section8";
+import { Section9 } from "./Section9";
+import { Section41 } from "./Section41";
 
 export default function Home() {
   return (
@@ -20,6 +27,27 @@ export default function Home() {
       <div>
         <Section3 />
       </div>
+      <div>
+        <Section41 />
+      </div>
+      <div>
+        <Section5 />
+      </div>
+      <div>
+        <Section6 />
+      </div>
+      <div>
+        <Section7 />
+      </div>
+      <div>
+        <Section8 />
+      </div>
+      <div>
+        <Section9 />
+      </div>
+      <br />
+      <br />
+      <br />
     </Sheet>
   );
 }
