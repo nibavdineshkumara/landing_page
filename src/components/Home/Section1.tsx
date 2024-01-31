1bc7a50b7fa6563cba544c1151844035e9a42fb5
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-01-31 11:30:32 +0530

    [Dinesh] ui changes in secion1

--- src/components/Home/Section1.tsx
+++ src/components/Home/Section1.tsx
@@ -1,8 +1,13 @@
-import { Box, Button, Divider, Stack, Typography } from "@mui/joy";
+import { Box, Button, Divider, Stack, Typography, useTheme } from "@mui/joy";
+import { useMediaQuery } from "@mui/material";
 
 export const Section1 = () => {
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
+  const aboveMobile = useMediaQuery(theme.breakpoints.up("xs"));
+  const belowTab = useMediaQuery(theme.breakpoints.down("sm"));
   return (
-    <Stack sx={{ maxHeight: "90dvh" }}>
+    <Stack sx={{ maxHeight: { xs: "100dvh", sm: "80dvh", md: "90dvh" } }}>
       <Stack
         sx={{
           px: { xs: 2, md: 10 },
@@ -13,10 +18,13 @@ export const Section1 = () => {
         spacing={10}
       >
         <Stack spacing={3} sx={{ flex: 10, zIndex: 4 }}>
-          <Typography level="h1" sx={{ fontSize: { xs: 32, md: 58 } }}>
+          <Typography
+            level="h1"
+            sx={{ fontSize: { xs: 32, md: 58 }, fontWeight: 600 }}
+          >
             Revolutionise Your Home with NIBAV Home Lifts in Switzerland!
           </Typography>
-          <Typography level="body-sm">
+          <Typography>
             Nibav Home Elevators: Luxurious and safe pneumatic technology for
             vertical transportation, designed with European safety standards and
             powered by abundant air.
@@ -40,21 +48,31 @@ export const Section1 = () => {
       <Stack
         sx={{
           position: "absolute",
-          top: { xs: 600, md: 290 },
+          top: { xs: 400, sm: 500, md: 290 },
+          bottom: 0,
           zIndex: 1,
           height: { xs: 900, md: 700 },
-          width: "100%",
-          overflow: "hidden",
+          width: { sm: "100%" },
+          overflow: "hidden !important",
         }}
       >
-        <img src="/mountain.svg" style={{ objectFit: "cover" }} />
+        <img
+          src="/mountain.svg"
+          style={{
+            objectFit: "cover",
+            ...(!isDesktop && {
+              height: belowTab ? "60%" : "auto",
+              width: "100%",
+            }),
+          }}
+        />
       </Stack>
       <Box
         sx={{
           position: "absolute",
           width: "100%",
           height: "700px",
-          top: { xs: 600, md: 290 },
+          top: { xs: 400, sm: 300, md: 290 },
           zIndex: 3,
           background:
             "linear-gradient(352.67deg, #201F25 26.92%, rgba(32, 31, 37, 0) 70.9%)",
@@ -63,7 +81,7 @@ export const Section1 = () => {
       <Stack
         sx={{
           position: "absolute",
-          top: { xs: 850, md: 600 },
+          top: { xs: 650, md: 600 },
           zIndex: 4,
           px: { xs: 2, md: 10 },
         }}
@@ -125,10 +143,14 @@ export const Counts = (props: CountsProps) => {
   return (
     <div>
       <Stack direction={"row"} sx={{ alignItems: "center" }} spacing={4}>
-        <Typography sx={{ fontSize: { xs: 40, sm: 64, color: "white" } }}>
+        <Typography
+          sx={{ fontSize: { xs: 40, sm: 64, color: "white" }, fontWeight: 600 }}
+        >
           {props?.count}+
         </Typography>
-        <Typography sx={{ color: "white" }}>{props?.caption}</Typography>
+        <Typography sx={{ color: "white", fontSize: { xs: 12, sm: 16 } }}>
+          {props?.caption}
+        </Typography>
       </Stack>
       <Divider sx={{ height: 2, backgroundColor: "gray" }} />
     </div>
