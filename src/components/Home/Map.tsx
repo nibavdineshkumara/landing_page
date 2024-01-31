6481cab688f696745b51b6e80a3c86a362bab2b1
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-01-31 11:30:13 +0530

    [Dinesh] adds Map iframe component

---
+++ src/components/Home/Map.tsx
@@ -0,0 +1,15 @@
+export const Map = () => {
+  return (
+    <>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2695.34922800077!2d8.764648376793334!3d47.50258969515352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479a995313cf2985%3A0x5b39133887b5034d!2sSulzerallee%2080%2C%208404%20Winterthur%2C%20Switzerland!5e0!3m2!1sen!2sin!4v1706617569524!5m2!1sen!2sin"
+        width="100%"
+        height="600"
+        style={{ border: 0 }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </>
+  );
+};
