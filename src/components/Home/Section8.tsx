fd4b56a2a416629692673f144e7d09481612ccb1
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-01-31 19:03:19 +0530

    [Dinesh] embeds testimonial videos

--- src/components/Home/Section8.tsx
+++ src/components/Home/Section8.tsx
@@ -1,26 +1,61 @@
-import { Button, Stack, Typography, useTheme } from "@mui/joy";
+import {
+  Box,
+  Button,
+  IconButton,
+  Modal,
+  ModalClose,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/joy";
 import { Hidden, useMediaQuery } from "@mui/material";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-const Recents = [
+import { PlayArrow } from "@mui/icons-material";
+const Reviews = [
   {
     id: 1,
-    src: "/reviews/1.mp4",
+    src: "https://fast.wistia.net/embed/iframe/zaeywt56o8?autoplay=1",
+    thumbnail:
+      "https://nibavlifts.com/wp-content/uploads/2022/06/Thumnalis-01.jpg",
   },
   {
     id: 2,
-    src: "/reviews/2.mp4",
+    src: "https://fast.wistia.net/embed/iframe/5g9fpjji84?autoplay=1",
+    thumbnail:
+      "https://nibavlifts.com/wp-content/uploads/2022/06/Thumnalis-02.jpg",
   },
   {
     id: 3,
-    src: "/reviews/3.mp4",
+    src: "https://fast.wistia.net/embed/iframe/70p896jy6t?autoplay=1",
+    thumbnail:
+      "https://nibavlifts.com/wp-content/uploads/2022/06/Thumnalis-03.jpg",
+  },
+  {
+    id: 4,
+    src: "https://fast.wistia.net/embed/iframe/bakiu2bjap?autoplay=1",
+    thumbnail:
+      "https://nibavlifts.com/wp-content/uploads/2022/06/Thumnalis-04.jpg",
+  },
+  {
+    id: 5,
+    src: "https://fast.wistia.net/embed/iframe/j4ppuz5d7o?autoplay=1",
+    thumbnail:
+      "https://nibavlifts.com/wp-content/uploads/2022/06/Thumnalis-05.jpg",
+  },
+  {
+    id: 6,
+    src: "https://fast.wistia.net/embed/iframe/uv9voiunhv?autoplay=1",
+    thumbnail:
+      "https://nibavlifts.com/wp-content/uploads/2022/06/Thumnalis-06.jpg",
   },
 ];
 export const Section8 = () => {
+  const [modalOpen, setModalOpen] = useState(false);
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
   return (
@@ -45,7 +80,7 @@ export const Section8 = () => {
           </Button>
         </Hidden>
       </Stack>
-      <>
+      <Box sx={{ px: { xs: 2, md: 10 } }}>
         <Swiper
           modules={[Navigation, Pagination]}
           spaceBetween={30}
@@ -53,15 +88,21 @@ export const Section8 = () => {
           navigation={true}
           style={{ width: "100%" }}
         >
-          {Recents?.map((recent) => (
-            <SwiperSlide style={{ height: "550px !important" }} key={recent.id}>
-              <div style={{ width: "100%", height: "550px" }}>
-                <Video src={recent.src} />
+          {Reviews?.map((review) => (
+            <SwiperSlide
+              style={{ width: "550px !important", height: "350px !important" }}
+              key={review.id}
+            >
+              <div style={{ width: "550px", height: "350px" }}>
+                <VideoWithThumbnail
+                  thumbnail={review.thumbnail}
+                  src={review.src}
+                />
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
-      </>
+      </Box>
       <Hidden mdUp>
         <Button variant="outlined" sx={{ alignSelf: "center" }}>
           View All
@@ -71,7 +112,93 @@ export const Section8 = () => {
   );
 };
 
-export function Video({ src }: { src: string }) {
+type Props = {
+  thumbnail: string;
+  src: string;
+};
+const VideoWithThumbnail = (props: Props) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <img
+        decoding="async"
+        src={props?.thumbnail}
+        alt=""
+        data-lazy-src={props?.thumbnail}
+        data-ll-status="loaded"
+        width={"100%"}
+        height={"100%"}
+      />
+      <IconButton
+        sx={{
+          position: "absolute",
+          top: "40%",
+          left: "40%",
+          bgcolor: "rgba(0, 0, 0, 0.5)",
+          borderRadius: "20%",
+          height: 80,
+          "&:hover": {
+            background: "rgba(0, 0, 0, 0.7)",
+          },
+        }}
+        onClick={() => {
+          setModalOpen(true);
+        }}
+        size="lg"
+      >
+        <PlayArrow sx={{ fontSize: 80 }} htmlColor="white" />
+      </IconButton>
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <Box sx={{ width: "100%", height: { md: "100%" } }}>
+          <ModalClose variant="solid" size="lg" sx={{ m: 1 }} />
+          <Box
+            sx={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <iframe
+              src={props.src}
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+            {/* <video
+              width="100%"
+              height="100%"
+              preload="auto"
+              controls
+              autoPlay
+              style={{ objectFit: "cover" }}
+            >
+              <source src={`${props.src}`} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video> */}
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+function Video({ src }: { src: string }) {
   const ref = useRef<any>(null);
   return (
     <video
