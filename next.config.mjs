8f2563e99f544e662856c463946cf55c764ae460
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-02-01 07:05:21 +0530

    [Dinesh] adds remote domain in next.config.js for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nibavlifts.com",
+        port: "",
+        pathname: "/wp-content/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
