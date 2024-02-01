a805604aa418484094c38765e90175b52902d869
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-02-01 07:23:36 +0530

    [Dinesh] preload variant images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Provider } from "@/components/Provider";
+import Image from "next/image";
 import * as React from "react";
 
 export const metadata = {
@@ -8,12 +9,40 @@ export const metadata = {
   description: "Now Luxury in Every Home",
 };
 
+const color_variants = [
+  "white",
+  "cream",
+  "black",
+  "gray",
+  "green",
+  "brown",
+  "blue",
+  "gold",
+  "silver",
+];
+const PreloadImages = () => {
+  return (
+    <>
+      {color_variants?.map((color) => (
+        <Image
+          src={`/color_variants/${color}.png`}
+          alt={color}
+          layout="fill"
+          priority
+          hidden
+          key={color}
+        />
+      ))}
+    </>
+  );
+};
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body style={{ margin: 0 }}>
         <Provider>
           <Header />
+          <PreloadImages />
           {props.children}
           <Footer />
         </Provider>
