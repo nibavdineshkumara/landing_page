45b2ba405509e30c61946cb547c3f1022c53e260
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-02-06 11:14:44 +0530

    [Dinesh] adds configurator page

---
+++ src/app/configurator/page.tsx
@@ -0,0 +1,7 @@
+import { Configurator } from "@/components/Configurator";
+
+const ConfiguratorPage = () => {
+  return <Configurator />;
+};
+
+export default ConfiguratorPage;
---
+++ src/components/Configurator/index.tsx
@@ -0,0 +1,5 @@
+import { Typography } from "@mui/joy";
+import { Canvas, useFrame } from "@react-three/fiber";
+export const Configurator = () => {
+  return <Typography>Configurator page</Typography>;
+};
